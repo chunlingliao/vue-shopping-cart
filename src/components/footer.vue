57ebc62cc0c16995edd912ab9d8740897b404869
dabbie <dabbie@mail.js-tech.tw>
2020-09-17 18:22:19 +0800

    footer拉出來寫成components

---
+++ src/components/footer.vue
@@ -0,0 +1,43 @@
+<template>
+  <footer class="footer">
+      <div class="footer-wrap pt-3 pb-3">
+          <div class="container">
+              <div class="row">
+                  <div class="col-sm-6 col-md-3">
+                      <h5>聯絡資訊</h5>
+                      <ul>
+                          <li>週一至週五 AM09:30~PM06:30(例假日休息)</li>
+                          <li>(02) 2933-1987</li>
+                          <li>test@gmail.com</li>
+                      </ul>
+                  </div>
+                  <div class="col-sm-6 col-md-3">
+                      <h5>關於我們</h5>
+                      <ul>
+                          <li>關於我們</li>
+                          <li>聯絡我們</li>
+                          <li>全部商品</li>
+                      </ul>
+                  </div>
+                  <div class="col-sm-6 col-md-3">
+                      <h5>常見問題</h5>
+                      <ul>
+                          <li>退貨/退款說明</li>
+                          <li>隱私政策</li>
+                      </ul>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
