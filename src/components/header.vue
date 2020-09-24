1652b094d561bcab8a55557ccff1c5714663c930
daisy <dai4116@gmail.com>
2020-09-24 17:24:51 +0800

    購物車下拉選單清單

--- src/components/header.vue
+++ src/components/header.vue
@@ -33,21 +33,24 @@
             <i class="fas fa-shopping-cart"></i></a>
           <div class="dropdown-menu dropdown-menu-right" aria-labelledby="shopcart-dropdown">
             <!-- 一項商品 -->
-            <div class="card-list">
+            <div class="card-list" v-for="item in itemList" :key="item.id">
               <div class="product-item">
                 <div class="thumb-img">
                   <img class="w-100" src="images/01.png" alt="">
                 </div>
                 <div class="product-name">
-                  <p class="title">MB-043 奧本口袋型電動刮鬍刀</p>
+                  <p class="title">{{ item.itemName }}</p>
                   <p class="amount">
-                    <span class="danger-color"> NT$489元 </span>
-                    <span>*1</span>
+                    <span class="danger-color"> <span>NT</span><span class="mr-1">{{ item.price }}</span> </span>
+                    <span>*{{ item.count }}</span>
                   </p>
                 </div>
               </div>
             </div>
-            <div class="total text-right mb-2"><span>總計: </span><span class="danger-color">1188</span></div>
+            <div class="total text-right mb-2">
+              <span>總計: </span>
+              <span class="danger-color">{{ listTotalAmount }}</span>
+            </div>
             <div class="btn btn-block btn-cart">立即結帳</div>
           </div>
         </div>
@@ -58,7 +61,75 @@
 
 <script>
   export default {
-
+    name: "",
+    props: {},
+    data: function () {
+      // 資料
+      return {
+        itemList: [{
+            id: '1',
+            itemName: 'MB-041 奧本水洗式電動鼻毛刀',
+            price: '489',
+            count: 1,
+          },
+          {
+            id: '2',
+            itemName: 'MB-045 奧本電池式電動除毛刀',
+            price: '699',
+            count: 1,
+          }
+        ],
+        listTotalAmount:'0',
+      };
+    },
+    components: {
+      // f2ecommonMask //1.個別引入
+    },
+    watch: {
+      //監聽值
+      
+    },
+    computed: {
+      //相依的資料改變時才做計算方法
+    },
+    methods: {
+      // 初始
+      totalprice(){
+        let total = 0
+          for (let i in this.itemList) {
+            total += this.itemList[i].price * this.itemList[i].count
+       }
+       this.listTotalAmount = total
+      }
+    },
+    //BEGIN--生命週期
+    beforeCreate: function () {
+      //實體初始化
+    },
+    created: function () {
+      //實體建立完成。資料 $data 已可取得，但 $el 屬性還未被建立。
+      this.src = this.$options.__file;
+    },
+    beforeMount: function () {
+      //執行元素掛載之前。
+    },
+    mounted: function () {
+      //元素已掛載， $el 被建立。
+      console.log(window.customElements)
+    },
+    beforeUpdate: function () {
+      //當資料變化時被呼叫，還不會描繪 View。
+    },
+    updated: function () {
+      //當資料變化時被呼叫，還不會描繪 View。
+    },
+    beforeDestroy: function () {
+      //實體還可使用。
+    },
+    destroyed: function () {
+      //實體銷毀。
+    }
+    //END--生命週期
   }
 </script>
 
