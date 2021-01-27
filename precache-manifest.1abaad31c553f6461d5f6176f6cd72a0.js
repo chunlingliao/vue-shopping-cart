6f4949e8a7785b99c8b38206d67739f1a1d2ad80
dabbie <dabbie@mail.js-tech.tw>
2021-01-27 14:09:48 +0800

    gh-pages

---
+++ precache-manifest.1abaad31c553f6461d5f6176f6cd72a0.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f910e79d2f79ef83882a2e55bf1ada0f",
+    "url": "/vue-shopping-cart/css/aos.css"
+  },
+  {
+    "revision": "0955d4bc28725e9aab0b",
+    "url": "/vue-shopping-cart/css/app.eaaec01c.css"
+  },
+  {
+    "revision": "f411c136e2bb302ada2120b3eb1d5bc3",
+    "url": "/vue-shopping-cart/css/bootstrap.min.css"
+  },
+  {
+    "revision": "ace8346ae5e3c577a21a",
+    "url": "/vue-shopping-cart/css/chunk-vendors.c6add683.css"
+  },
+  {
+    "revision": "d251717d16124709bbec259599ad2858",
+    "url": "/vue-shopping-cart/css/style.css"
+  },
+  {
+    "revision": "9f92061e59a623b270f1f0c36a451f5d",
+    "url": "/vue-shopping-cart/images/01.png"
+  },
+  {
+    "revision": "6feb4c61729beb169f9b2c6a4700ca8f",
+    "url": "/vue-shopping-cart/img/Lipstick/z001.jpg"
+  },
+  {
+    "revision": "d32bee1928baeb9ce5c45365f9761c6f",
+    "url": "/vue-shopping-cart/img/Lipstick/z002.jpg"
+  },
+  {
+    "revision": "2b787810fc9e2010c3a68f2156f1bd4a",
+    "url": "/vue-shopping-cart/img/Lipstick/z003.jpg"
+  },
+  {
+    "revision": "4f8cac6e2b445bb5baad289a9eb73b72",
+    "url": "/vue-shopping-cart/img/Lipstick/z004.png"
+  },
+  {
+    "revision": "134120a2d61dcdcca09043b3a371a283",
+    "url": "/vue-shopping-cart/img/Lipstick/z005.jpg"
+  },
+  {
+    "revision": "4f70a25afbd44647f97d84a60625bac3",
+    "url": "/vue-shopping-cart/img/Lipstick/z006.jpg"
+  },
+  {
+    "revision": "e60f8b52e279a566e98c8d1b949a20c9",
+    "url": "/vue-shopping-cart/img/Lipstick/z007.jpg"
+  },
+  {
+    "revision": "731c7e5fecea6077b26687b23dd67e85",
+    "url": "/vue-shopping-cart/img/Lipstick/z008.jpg"
+  },
+  {
+    "revision": "e9521a8e9e9577f5d6145e2500820c14",
+    "url": "/vue-shopping-cart/img/Lipstick/z009.jpg"
+  },
+  {
+    "revision": "908d82c3b27f8bf001d3ea72e04ae195",
+    "url": "/vue-shopping-cart/img/Lipstick/z010.jpg"
+  },
+  {
+    "revision": "fa9711a67fb02ee679d299f32e450e07",
+    "url": "/vue-shopping-cart/img/Lipstick/z011.jpg"
+  },
+  {
+    "revision": "3edab9eaf511da95b72cd7d61ceba9c6",
+    "url": "/vue-shopping-cart/img/Lipstick/z012.jpg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-shopping-cart/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "949988ff03f60cfb22e59c7b8735e7fb",
+    "url": "/vue-shopping-cart/index.html"
+  },
+  {
+    "revision": "a01f9089e8301e9eacfb9d029dc0ca5c",
+    "url": "/vue-shopping-cart/js/aos.js"
+  },
+  {
+    "revision": "0955d4bc28725e9aab0b",
+    "url": "/vue-shopping-cart/js/app.e3318113.js"
+  },
+  {
+    "revision": "eb5fac582a82f296aeb74900b01a2fa3",
+    "url": "/vue-shopping-cart/js/bootstrap.min.js"
+  },
+  {
+    "revision": "73cb019bdb303efd0bad",
+    "url": "/vue-shopping-cart/js/chunk-2d0b68f8.20391fc2.js"
+  },
+  {
+    "revision": "1b26d763522de8a0a866",
+    "url": "/vue-shopping-cart/js/chunk-2d229481.8cc507ca.js"
+  },
+  {
+    "revision": "064c42bea8a79b0da300",
+    "url": "/vue-shopping-cart/js/chunk-5bd0b9aa.8ed71c72.js"
+  },
+  {
+    "revision": "7922925d572a8e90046a",
+    "url": "/vue-shopping-cart/js/chunk-6473fc7e.17ca0059.js"
+  },
+  {
+    "revision": "888d2dd6c0c0480ed616",
+    "url": "/vue-shopping-cart/js/chunk-7173126a.aec8bace.js"
+  },
+  {
+    "revision": "ace8346ae5e3c577a21a",
+    "url": "/vue-shopping-cart/js/chunk-vendors.19faf068.js"
+  },
+  {
+    "revision": "a09e13ee94d51c524b7e2a728c7d4039",
+    "url": "/vue-shopping-cart/js/jquery-3.3.1.min.js"
+  },
+  {
+    "revision": "83fb8c4d9199dce0224da0206423106f",
+    "url": "/vue-shopping-cart/js/popper.min.js"
+  },
+  {
+    "revision": "fe3b9bce8d930e0d5fb01aadacd1c01c",
+    "url": "/vue-shopping-cart/js/swiper.min.js"
+  },
+  {
+    "revision": "ef001587ca0a02e225f065ea15fd621f",
+    "url": "/vue-shopping-cart/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-shopping-cart/robots.txt"
+  }
+]);
